44cbd2052b1d27e4f0982e2d01b69d13031ade07
Jah0ng1r <rajabaliyevjahongir2@gmail.com>
2022-12-01 00:23:31 +0500

    Done

---
+++ spring Security with JDBC/src/main/java/uz/fbtuit/spring_security_with_jdbc/SpringSecurityWithJdbcApplication.java
@@ -0,0 +1,13 @@
+package uz.fbtuit.spring_security_with_jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityWithJdbcApplication.class, args);
+    }
+
+}
---
+++ spring Security with JDBC/src/test/java/uz/fbtuit/spring_security_with_jdbc/SpringSecurityWithJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.fbtuit.spring_security_with_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
